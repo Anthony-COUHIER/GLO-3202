28a08a234fbd90714021a010e09f3cc009300dfe
Anthony COUHIER <anthony.couhier@epitech.eu>
2023-02-11 13:20:24 -0500

    fix: deployement

--- src/env/schema.ts
+++ src/env/schema.ts
@@ -8,13 +8,7 @@ export const serverScheme = z.object({
         .default('false')
         .transform((value) => value === 'true'),
     DATABASE_URL: z.string(),
-    START_BASE_URL: z.preprocess(
-        // This makes Vercel deployments not fail if you don't set START_BASE_URL
-        // Since NextAuth.js automatically uses the VERCEL_URL if present.
-        (str) => process.env.VERCEL_URL ?? str,
-        // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-        process.env.VERCEL ? z.string() : z.string().url(),
-    ),
+    START_BASE_URL: z.string().optional().default('https://glo-3202.vercel.app'),
     AUTH_SECRET: z.string(),
 })
 

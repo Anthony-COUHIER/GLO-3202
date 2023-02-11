d68df7b45552a77c02a1f21f60d64cfa59f2c726
Anthony COUHIER <anthony.couhier@epitech.eu>
2023-02-11 13:01:51 -0500

    Revert "fix: call error"
    This reverts commit fa353b48f021d506936941ad1a24fdeb99a41b63.

--- src/env/schema.ts
+++ src/env/schema.ts
@@ -20,11 +20,5 @@ export const serverScheme = z.object({
 
 export const clientScheme = z.object({
     MODE: z.enum(['development', 'production', 'test']).default('development'),
-    START_BASE_URL:  z.preprocess(
-        // This makes Vercel deployments not fail if you don't set START_BASE_URL
-        // Since NextAuth.js automatically uses the VERCEL_URL if present.
-        (str) => process.env.VERCEL_URL ?? str,
-        // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-        process.env.VERCEL ? z.string() : z.string().url(),
-    ),
+    START_BASE_URL: z.string().optional().default('http://127.0.0.1:3000'),
 })

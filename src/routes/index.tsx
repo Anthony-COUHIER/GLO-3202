22daaf8951c07210f753cb2421864ccb29a82a11
Anthony COUHIER <anthony.couhier@epitech.eu>
2023-02-11 13:50:40 -0500

    fix: login componentn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,9 +4,5 @@ import { useAuth } from "~/context/auth";
 export default function Home() {
   const { user } = useAuth();
 
-  if (!user()) {
-    <Navigate href="/login" />;
-  } else {
-    <Navigate href="/dashboard" />;
-  }
+  <Navigate href="/dashboard" />;
 }
